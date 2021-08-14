6da5dc0619cc2f4301ac8c72a83e00f174ae560c
Kai <73109076+alienflip@users.noreply.github.com>
2021-08-14 17:04:17 +0100

    Update SudukuThreadPool.c

--- multithread/SudukuThreadPool.c
+++ multithread/SudukuThreadPool.c
@@ -5,10 +5,14 @@
 #include <unistd.h>
 
 #define rowThreadNum 2
+#define colThreadNum 2
+#define squareThreadNum 2
 
 #define squareUnit 2
 #define square 4
 int** input;
+
+int isValid = 1;
 
 /*
 typedef struct TaskCol {
@@ -64,10 +68,80 @@ int checkSquare(int input[][square], int subX, int subY){
 */
 
 // threadpool for row calculations
+typedef struct taskCol {
+  int** input;
+  int index;
+} taskCol;
+taskCol colTaskQueue[square];
+int colTaskCount = 0;
+
+void checkCol(taskCol* task){
+  int** input = task -> input;
+  int index = task -> index;
+  int available[] = {0,0,0,0,0,0,0,0,0,0};
+  for(int i = 0; i < square; i++){
+
+    if(input[i][index] == -1){
+      continue;
+    }
+    else{
+      available[input[i][index]]++;
+      if(available[input[i][index]] > 1){
+        isValid = 0;
+        printf("index: %d fails\n", index);
+        return;
+      }
+    }
+    for(int j = 0; j < 9; j++){
+      printf("%d ", available[j]);
+    }printf("\n");
+  }
+  printf("index: %d passes\n", index);
+  return;
+}
+
+pthread_mutex_t colMutexQueue;
+pthread_cond_t colCondQueue;
+
+void* colStartThread(void* args){
+  while(1){
+    taskCol task;
+    pthread_mutex_lock(&colMutexQueue);
+    // waits for task to become available
+    while(colTaskCount == 0){
+      pthread_cond_wait(&colCondQueue, &colMutexQueue);
+    }
+    // choose task
+    task = colTaskQueue[0];
+    int i;
+    for(i = 0; i < colTaskCount; i++){
+      colTaskQueue[i] = colTaskQueue[i + 1];
+    }
+    colTaskCount--;
+    pthread_mutex_unlock(&colMutexQueue);
+    // execute task
+    checkCol(&task);
+    if(colTaskCount == 0){
+      break;
+    }
+  }
+}
+
+void submitColTask(taskCol task){
+  pthread_mutex_lock(&colMutexQueue);
+  colTaskQueue[colTaskCount] = task;
+  colTaskCount++;
+  pthread_mutex_unlock(&colMutexQueue);
+  pthread_cond_signal(&colCondQueue);
+}
+
+/*
+//
+// threadpool for row calculations
+//
 typedef struct taskRow {
   int** input;
   int index;
-  //int type;
 } taskRow;
 taskRow rowTaskQueue[square];
 int rowTaskCount = 0;
@@ -83,6 +157,7 @@ void checkRow(taskRow* task){
     else{
       available[input[index][i]]++;
       if(available[input[index][i]] > 1){
+        isValid = 0;
         printf("index: %d fails\n", index);
         return;
       }
@@ -98,12 +173,10 @@ pthread_cond_t rowCondQueue;
 void* rowStartThread(void* args){
   while(1){
     taskRow task;
-    int found = 0;
     pthread_mutex_lock(&rowMutexQueue);
     // waits for task to become available
     while(rowTaskCount == 0){
       pthread_cond_wait(&rowCondQueue, &rowMutexQueue);
-
     }
     // choose task
     task = rowTaskQueue[0];
@@ -115,6 +188,9 @@ void* rowStartThread(void* args){
     pthread_mutex_unlock(&rowMutexQueue);
     // execute task
     checkRow(&task);
+    if(rowTaskCount == 0){
+      break;
+    }
   }
 }
 
@@ -125,6 +201,7 @@ void submitRowTask(taskRow task){
   pthread_mutex_unlock(&rowMutexQueue);
   pthread_cond_signal(&rowCondQueue);
 }
+*/
 
 int main(void){
 
@@ -133,9 +210,9 @@ int main(void){
   //
   int arrayIn[][square] = {
       {1,-1,-1,2},
-      {2,-1,1,1},
+      {2,-1,1,-1},
       {0,3,-1,-1},
-      {-1,-1,2,2}
+      {-1,-1,-1,2}
     };
 
   input = malloc(square * sizeof(int*));
@@ -146,6 +223,9 @@ int main(void){
     }
   }
 
+  /*
+  //
+  // row threadpool execute
   //
   pthread_t th_row[rowThreadNum];
   pthread_mutex_init(&rowMutexQueue, NULL);
@@ -156,11 +236,11 @@ int main(void){
     }
   }
   for(int i = 0; i < square; i++){
-    taskRow t1 = {
+    taskRow t = {
       .input = input,
       .index = i
     };
-    submitRowTask(t1);
+    submitRowTask(t);
   }
   for(int i = 0; i < rowThreadNum; i++){
     if(pthread_join(th_row[i], NULL) != 0){
@@ -169,16 +249,35 @@ int main(void){
   }
   pthread_mutex_destroy(&rowMutexQueue);
   pthread_cond_destroy(&rowCondQueue);
+  */
 
-  /*
-  for(int i = 0; i < square; i++){
-    isValid = isValid&&checkRow(input, i);
-    if(isValid == 0){
-      printf("%d\n", isValid);
-      return isValid;
+  //
+  // col threadpool execute
+  //
+  pthread_t th_col[colThreadNum];
+  pthread_mutex_init(&colMutexQueue, NULL);
+  pthread_cond_init(&colCondQueue, NULL);
+  for(int i = 0; i < colThreadNum; i++){
+    if(pthread_create(&th_col[i], NULL, &colStartThread, NULL) != 0){
+      printf("failed to create thread\n");
     }
   }
+  for(int i = 0; i < square; i++){
+    taskCol t = {
+      .input = input,
+      .index = i
+    };
+    submitColTask(t);
+  }
+  for(int i = 0; i < colThreadNum; i++){
+    if(pthread_join(th_col[i], NULL) != 0){
+      printf("failed to join thread\n");
+    }
+  }
+  pthread_mutex_destroy(&colMutexQueue);
+  pthread_cond_destroy(&colCondQueue);
 
+  /*
   int subX = 0;
   int subY = 0;
   for(int i = 0; i < squareUnit; i++){
@@ -194,7 +293,14 @@ int main(void){
     subX = subX + squareUnit;
   }
   */
-
+  switch(isValid){
+    case 0:
+      printf("does not pass\n");
+      break;
+    case 1:
+      printf("passes\n");
+      break;
+  }
   free(input);
   return 1;
 }
