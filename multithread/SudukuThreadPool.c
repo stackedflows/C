6b3b4745a2989635be5b6fae3821177ab90bbd24
Kai <73109076+alienflip@users.noreply.github.com>
2021-08-14 12:38:50 +0100

    Update SudukuThreadPool.c

--- multithread/SudukuThreadPool.c
+++ multithread/SudukuThreadPool.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <pthread.c>
-#include <unistd.h>
+//#include <pthread.c>
+//#include <unistd.h>
 
 #define square 4
+#define squareUnit floor(sqrt(square))
 
-int checkRow(int input[][square],  int index, int width){
+//
+// tasks for threads to complete
+//
+typedef struct TaskRow {
+  int index;
+  int input[][square];
+} TaskRow;
+TaskRow rowTaskQueue[256];
+int rowTaskCount = 0;
+
+typedef struct TaskCol {
+  int index;
+  int input[][square];
+} TaskCol;
+TaskCol colTaskQueue[256];
+int colTaskCount = 0;
+
+typedef struct TaskSquare {
+  int subX;
+  int subY;
+  int input[][square];
+} TaskSquare;
+TaskSquare squareTaskQueue[256];
+int squareTaskCount = 0;
+
+//
+// executionable tasks
+//
+int checkRow(int input[][square], int index){
   int available[] = {0,0,0,0,0,0,0,0,0,0};
-  for(int i = 0; i < width; i++){
+  for(int i = 0; i < square; i++){
     if(input[index][i] == -1){
       continue;
     }
@@ -22,9 +51,9 @@ int checkRow(int input[][square],  int index, int width){
   return 1;
 }
 
-int checkCol(int input[][square], int index, int height){
+int checkCol(int input[][square], int index){
   int available[] = {0,0,0,0,0,0,0,0,0,0};
-  for(int i = 0; i < height; i++){
+  for(int i = 0; i < square; i++){
     if(input[i][index] == -1){
       continue;
     }
@@ -38,10 +67,10 @@ int checkCol(int input[][square], int index, int height){
   return 1;
 }
 
-int checkSquare(int input[][square], int subX, int subY, int subWidth){
+int checkSquare(int input[][square], int subX, int subY){
   int available[] = {0,0,0,0,0,0,0,0,0,0};
-  for(int i = subX; i < subX + subWidth; i++){
-    for(int j = subY; j < subY + subWidth; j++){
+  for(int i = subX; i < subX + squareUnit; i++){
+    for(int j = subY; j < subY + squareUnit; j++){
       if(input[i][j] == -1){
         continue;
       }
@@ -56,24 +85,26 @@ int checkSquare(int input[][square], int subX, int subY, int subWidth){
   return 1;
 }
 
+//
 // total number of independent processes here to finish is 3 * square
-int validate(int input[][square], int width, int height){
+//
+int validate(int input[][square]){
   int isValid = 1;
-  for(int i = 0; i < height; i++){
-    isValid&&checkRow(input, i, width);
+  for(int i = 0; i < square; i++){
+    isValid&&checkRow(input, i);
     if(isValid == 0) return isValid;
   }
-  for(int i = 0; i < width; i++){
-    isValid&&checkCol(input, i, height);
+  for(int i = 0; i < square; i++){
+    isValid&&checkCol(input, i);
     if(isValid == 0) return isValid;
   }
-  int squareUnit = floor(sqrt(square));
+
   int subX = 0;
   int subY = 0;
   for(int i = 0; i < squareUnit; i++){
     subY = 0;
     for(int j = 0; j < squareUnit; j++){
-      isValid&&checkSquare(input, subX, subY, squareUnit);
+      isValid&&checkSquare(input, subX, subY);
       if(isValid == 0) return isValid;
       subY = subY + squareUnit;
     }
@@ -82,6 +113,9 @@ int validate(int input[][square], int width, int height){
   return isValid;
 }
 
+//
+// approach: leave 2 threads in 3 thread pools, and make the task queue of length 3 * square
+//
 int main(void){
 
   int input0[][square] = {
@@ -94,7 +128,7 @@ int main(void){
   int width = sizeof(input0)/sizeof(input0[0]);
   int height = sizeof(input0[0])/sizeof(int);
 
-  int k = validate(input0, square, square);
+  int k = validate(input0);
   printf("%d\n", k);
   return 0;
 }
