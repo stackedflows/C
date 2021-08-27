93956539c82c3b8d8b893068a1971e53ddbc589c
Kai <73109076+alienflip@users.noreply.github.com>
2021-08-27 18:34:40 +0100

    Update Main.cpp

--- graphics/Main.cpp
+++ graphics/Main.cpp
@@ -1,5 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <shader.h>
 #include <iostream>
 #include <stdlib.h>
